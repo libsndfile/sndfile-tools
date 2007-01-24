7fcd624d73481b99fedfd92d678a0f7ec46eee8b
Erik de Castro Lopo <erikd@mega-nerd.com>
2007-01-24 21:16:28 +1100

    sndfile-spectrogram.c : Add GPL notice.

--- sndfile-spectrogram.c
+++ sndfile-spectrogram.c
@@ -1,3 +1,24 @@
+/*
+** Copyright (C) 2007 Erik de Castro Lopo <erikd@mega-nerd.com>
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; version 2 of the License only.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+/*
+**	Generate a spectrogram as a PNG file from a given sound file.
+*/
+
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
