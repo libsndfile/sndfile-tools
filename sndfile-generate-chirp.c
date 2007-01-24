f998c7f041b3fdf91997786e80a23236a718595a
Erik de Castro Lopo <erikd@mega-nerd.com>
2007-01-24 21:59:45 +1100

    Updates on sndfile-generate-chirp.c, update .bzrignore.

--- sndfile-generate-chirp.c
+++ sndfile-generate-chirp.c
@@ -19,9 +19,84 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 #include <math.h>
 
 #include <sndfile.h>
+
+static void
+write_log_chirp (SNDFILE * file, int samplerate, int seconds)
+{
+	float * data ;
+	int sec ;
+
+	data = malloc (samplerate * sizeof (data [0])) ;
+	if (data == NULL)
+	{	printf ("\nError : malloc failed : %s\n", strerror (errno)) ;
+		exit (1) ;
+		} ;
+
+	for (sec = 0 ; sec < seconds ; sec ++)
+	{
+		sf_write_float (file, data, samplerate) ;
+		} ;
+
+	free (data) ;
+} /* write_log_chirp */
+
+static void
+generate_file (const char * filename, int format, int samplerate, int seconds)
+{
+	SNDFILE * file ;
+	SF_INFO info ;
+
+	memset (&info, 0, sizeof (info)) ;
+
+	info.format = format ;
+	info.samplerate = samplerate ;
+	info.channels = 1 ;
+
+	file = sf_open (filename, SFM_WRITE, &info) ;
+	if (file == NULL)
+	{	printf ("\nError : Not able to create file named '%s' : %s/\n", filename, sf_strerror (NULL)) ;
+		exit (1) ;
+		} ;
+
+	sf_set_string (file, SF_STR_TITLE, filename) ;
+	sf_set_string (file, SF_STR_SOFTWARE, "sndfile-generate-chirp") ;
+	sf_set_string (file, SF_STR_COPYRIGHT, "No copyright.") ;
+
+	write_log_chirp (file, samplerate, seconds) ;
+
+	sf_close (file) ; 
+} /* generate_file */
+
+static int
+guess_major_format (const char * filename)
+{
+	const char * ext ;
+
+	ext = strrchr (filename, '.') ;
+	if (ext == NULL)
+	{	printf ("\nError : cannot figure out file type from file name '%s'.\n\n", filename) ;
+		exit (1) ;
+		} ;
+
+	if (strcasecmp (ext, ".wav") == 0)
+		return SF_FORMAT_WAV ;
+	if (strcasecmp (ext, ".aif") == 0 || strcasecmp (ext, ".aiff") == 0)
+		return SF_FORMAT_AIFF ;
+	if (strcasecmp (ext, ".au") == 0)
+		return SF_FORMAT_AU ;
+	if (strcasecmp (ext, ".caf") == 0)
+		return SF_FORMAT_CAF ;
+	if (strcasecmp (ext, ".w64") == 0)
+		return SF_FORMAT_W64 ;
+
+	printf ("\nError : Can only generate files with extentions 'wav', 'aiff', 'aif', 'au', 'w64' and 'caf'.\n\n") ;
+	exit (1) ;
+	return 0 ;
+} /* guess_major_format */
 
 static void
 check_int_range (const char * name, int value, int lower, int upper)
@@ -42,9 +117,7 @@ usage_exit (const char * argv0)
 
 	printf ("\nUsage :\n\n    %s <sample rate> <length in seconds> <sound file>\n\n", progname) ;
 
-	puts (
-		"    Create containing a logarithmic chirp.\n"
-		) ;
+	puts ("    Create a file containing a logarithmic chirp.\n") ;
 
 	exit (0) ;
 } /* usage_exit */
@@ -52,16 +125,22 @@ usage_exit (const char * argv0)
 int
 main (int argc, char * argv [])
 {
-	int samplerate, seconds ;
+	const char * filename ;
+	int samplerate, seconds, format ;
 
 	if (argc != 4)
 		usage_exit (argv [0]) ;
 
-	samplerate = atoi (argv [2]) ;
-	seconds = atoi (argv [3]) ;
+	samplerate = atoi (argv [1]) ;
+	seconds = atoi (argv [2]) ;
+	filename = argv [3] ;
 
 	check_int_range ("sample rate", samplerate, 1000, 200 * 1000) ;
 	check_int_range ("seconds", seconds, 1, 1000) ;
+
+	format = guess_major_format (filename) | SF_FORMAT_FLOAT ;
+
+	generate_file (filename, format, samplerate, seconds) ;
 
 	return 0 ;
 } /* main */
