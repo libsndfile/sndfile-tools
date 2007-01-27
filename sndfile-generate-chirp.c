4d0a4d0be84d3de3881dca1db9bcd49c57fa4f50
Erik de Castro Lopo <erikd@mega-nerd.com>
2007-01-28 10:21:30 +1100

    sndfile-generate-chirp.c : Add multiple sweep types (quadratic not working yet).

--- sndfile-generate-chirp.c
+++ sndfile-generate-chirp.c
@@ -24,31 +24,36 @@
 
 #include <sndfile.h>
 
+typedef double (*freq_func_t) (double w0, double w1, double total_length) ;
+
 static void usage_exit (const char * argv0) ;
-static void generate_file (const char * filename, int format, int samplerate, int seconds) ;
 static void check_int_range (const char * name, int value, int lower, int upper) ;
+static freq_func_t parse_sweep_type (const char * name) ;
 static int guess_major_format (const char * filename) ;
+static void generate_file (const char * filename, int format, int samplerate, int seconds, freq_func_t sweep_func) ;
 
 
 int
 main (int argc, char * argv [])
 {
 	const char * filename ;
+	freq_func_t sweep_func ;
 	int samplerate, seconds, format ;
 
-	if (argc != 4)
+	if (argc != 5)
 		usage_exit (argv [0]) ;
 
-	samplerate = atoi (argv [1]) ;
-	seconds = atoi (argv [2]) ;
-	filename = argv [3] ;
+	sweep_func = parse_sweep_type (argv [1]) ;
+	samplerate = atoi (argv [2]) ;
+	seconds = atoi (argv [3]) ;
+	filename = argv [4] ;
 
 	check_int_range ("sample rate", samplerate, 1000, 200 * 1000) ;
 	check_int_range ("seconds", seconds, 1, 1000) ;
 
 	format = guess_major_format (filename) | SF_FORMAT_FLOAT ;
 
-	generate_file (filename, format, samplerate, seconds) ;
+	generate_file (filename, format, samplerate, seconds, sweep_func) ;
 
 	return 0 ;
 } /* main */
@@ -64,9 +69,22 @@ usage_exit (const char * argv0)
 	progname = strrchr (argv0, '/') ;
 	progname = (progname == NULL) ? argv0 : progname + 1 ;
 
-	printf ("\nUsage :\n\n    %s <sample rate> <length in seconds> <sound file>\n\n", progname) ;
+	puts ("\nCreate a sound file containing a swept sine wave (ie a chirp).") ;
 
-	puts ("    Create a file containing a logarithmic chirp.\n") ;
+	printf ("\nUsage :\n\n    %s <sweep type> <sample rate> <length in seconds> <sound file>\n\n", progname) ;
+
+	puts (
+		"    Where <sweep type> is one of :\n"
+		"           -log      logarithmic sweep\n"
+		"           -qrad     quadratic sweep\n"
+		"           -linear   linear sweep\n"
+		) ;
+
+	puts (
+		"    The output file will contain floating point samples in the range [-1.0, 1.0].\n"
+		"    The ouput file type is determined by the file name extension which should be one\n"
+		"    of 'wav', 'aif', 'aiff', 'au', 'caf' and 'w64'.\n"
+		) ;
 
 	exit (0) ;
 } /* usage_exit */
@@ -80,10 +98,11 @@ check_int_range (const char * name, int value, int lower, int upper)
 		} ;
 } /* check_int_range */
 
+
 static void
-write_log_chirp (SNDFILE * file, int samplerate, int seconds)
+write_chirp (SNDFILE * file, int samplerate, int seconds, double w0, double w1, freq_func_t sweep_func)
 {
-	double w0, w1, log10_w0, log10_w1, total_samples ;
+	double total_samples ;
 	double instantaneous_w, current_phase ;
 	float * data ;
 	int sec, k ;
@@ -94,20 +113,12 @@ write_log_chirp (SNDFILE * file, int samplerate, int seconds)
 		exit (1) ;
 		} ;
 
-	w0 = 2.0 * M_PI * 200.0 / samplerate ;
-	w1 = 2.0 * M_PI * 0.5 ;
-
-	log10_w0 = log10 (w0) ;
-	log10_w1 = log10 (w1) ;
-
 	total_samples = (1.0 * seconds) * samplerate ;
-
-	printf ("w0 : %0.4f    w1 : %0.4f\n", w0, w1) ;
 
 	current_phase = 0.0 ;
 	instantaneous_w = w0 ;
 
-	printf ("instantaneous angluar freq : %f (%7.1f Hz)\n", instantaneous_w, instantaneous_w * samplerate / (2.0 * M_PI)) ;
+	printf ("Start frequency : %8.1f Hz (%f rad/sec)\n", instantaneous_w * samplerate / (2.0 * M_PI), instantaneous_w) ;
 
 	for (sec = 0 ; sec < seconds ; sec ++)
 	{	for (k = 0 ; k < samplerate ; k++)
@@ -116,23 +127,24 @@ write_log_chirp (SNDFILE * file, int samplerate, int seconds)
 			data [k] = sin (current_phase) ;
 
 			current = sec * samplerate + k ;
-			instantaneous_w = pow (10.0, log10_w0 + (log10_w1 - log10_w0) * current / total_samples) ;
+			instantaneous_w = sweep_func (w0, w1, current / total_samples) ;
 			current_phase = fmod (current_phase + instantaneous_w, 2.0 * M_PI) ;
 			} ;
 
 		sf_write_float (file, data, samplerate) ;
 		} ;
 
-	printf ("instantaneous angluar freq : %f (%7.1f Hz)\n", instantaneous_w, instantaneous_w * samplerate / (2.0 * M_PI)) ;
+	printf ("End   frequency : %8.1f Hz (%f rad/sec)\n", instantaneous_w * samplerate / (2.0 * M_PI), instantaneous_w) ;
 
 	free (data) ;
-} /* write_log_chirp */
+} /* write_linear_chirp */
 
 static void
-generate_file (const char * filename, int format, int samplerate, int seconds)
+generate_file (const char * filename, int format, int samplerate, int seconds, freq_func_t sweep_func)
 {
 	SNDFILE * file ;
 	SF_INFO info ;
+	double w0, w1 ;
 
 	memset (&info, 0, sizeof (info)) ;
 
@@ -150,10 +162,46 @@ generate_file (const char * filename, int format, int samplerate, int seconds)
 	sf_set_string (file, SF_STR_SOFTWARE, "sndfile-generate-chirp") ;
 	sf_set_string (file, SF_STR_COPYRIGHT, "No copyright.") ;
 
-	write_log_chirp (file, samplerate, seconds) ;
+	w0 = 2.0 * M_PI * 200.0 / samplerate ;
+	w1 = 2.0 * M_PI * 0.5 ;
+
+	write_chirp (file, samplerate, seconds, w0, w1, sweep_func) ;
 
 	sf_close (file) ; 
 } /* generate_file */
+
+static double
+log_freq_func (double w0, double w1, double indx)
+{	return pow (10.0, log10 (w0) + (log10 (w1) - log10 (w0)) * indx) ;
+} /* log_freq_func */
+
+static double
+quad_freq_func (double w0, double w1, double indx)
+{
+	puts ("quad_freq_func not working yet.") ;
+	exit (1) ;
+	return pow (10.0, log10 (w0) + (log10 (w1) - log10 (w0)) * indx) ;
+} /* log_freq_func */
+
+static double
+linear_freq_func (double w0, double w1, double indx)
+{	return w0 + (w1 - w0) * indx ;
+} /* linear_freq_func */
+
+static freq_func_t
+parse_sweep_type (const char * name)
+{
+	if (strcmp (name, "-log") == 0)
+		return log_freq_func ;
+	if (strcmp (name, "-quad") == 0)
+		return quad_freq_func ;
+	if (strcmp (name, "-linear") == 0)
+		return linear_freq_func ;
+
+	printf ("\nError : Bad sweep type. Should be one of '-log', '-quad' and '-linear'.\n\n") ;
+	exit (1) ;
+	return NULL ;
+} /* parse_sweep_type */
 
 static int
 guess_major_format (const char * filename)
