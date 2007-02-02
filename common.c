76bac0689d1b4b533ca94d5279961f44cb5808c2
Erik de Castro Lopo <erikd@mega-nerd.com>
2007-02-02 18:35:55 +1100

    Add files common.[ch]*.

---
+++ common.c
@@ -0,0 +1,58 @@
+/*
+** Copyright (C) 2007 Erik de Castro Lopo <erikd@mega-nerd.com>
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; version 2 of the License only.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+#include <string.h>
+
+#include "common.h"
+
+sf_count_t
+sfe_mix_mono_read_double (SNDFILE * file, double * data, sf_count_t datalen)
+{
+	SF_INFO info ;
+
+	sf_get_info (file, &info) ;
+
+	if (info.channels == 1)
+		return sf_read_double (file, data, datalen) ;
+
+	static double multi_data [2048] ;
+	int k, ch, frames_read ;
+	sf_count_t dataout = 0 ;
+
+	while (dataout < datalen)
+	{	int this_read ;
+		
+		this_read = MIN (ARRAY_LEN (multi_data) / info.channels, datalen) ;
+
+		frames_read = sf_readf_double (file, multi_data, this_read) ;
+		if (frames_read == 0)
+			break ;
+
+		for (k = 0 ; k < frames_read ; k++)
+		{	double mix = 0.0 ;
+
+			for (ch = 0 ; ch < info.channels ; ch++)
+				mix += multi_data [k * info.channels + ch] ;
+			data [dataout + k] = mix / info.channels ;
+			} ;
+
+		dataout += this_read ;
+		} ;
+
+	return dataout ;
+} /* sfe_mix_mono_read_double */
+
---
+++ common.h
@@ -0,0 +1,25 @@
+/*
+** Copyright (C) 2007 Erik de Castro Lopo <erikd@mega-nerd.com>
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; version 2 of the License only.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+
+#include <sndfile.h>
+
+#define ARRAY_LEN(x)	((int) (sizeof (x) / sizeof (x [0])))
+#define MAX(x,y)		((x) > (y) ? (x) : (y))
+#define MIN(x,y)		((x) < (y) ? (x) : (y))
+
+sf_count_t sfe_mix_mono_read_double (SNDFILE * file, double * data, sf_count_t datalen) ;
