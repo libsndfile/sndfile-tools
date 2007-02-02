34fd51ae6b9a51c6113ff654b59d26e7188c1e5b
Erik de Castro Lopo <erikd@mega-nerd.com>
2007-02-02 18:38:50 +1100

    Add new files.

---
+++ sndfile-mix-to-mono.c
@@ -0,0 +1,94 @@
+/*
+** Copyright (C) 2007 Erik de Castro Lopo <erikd@mega-nerd.com>
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; version 2 of the License only.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include "common.h"
+
+static void usage_exit (void) ;
+static void mix_to_mono (SNDFILE * infile, SNDFILE * outfile) ;
+
+int
+main (int argc, char ** argv)
+{
+	SNDFILE *infile, *outfile ;
+	SF_INFO sfinfo ;
+
+	if (argc != 3)
+		usage_exit () ;
+
+	if (strcmp (argv [argc - 2], argv [argc - 1]) == 0)
+	{	printf ("Error : input and output file names are the same.\n") ;
+		exit (1) ;
+		} ;
+
+	/* Delete the output file length to zero if already exists. */
+	remove (argv [argc - 1]) ;
+
+	memset (&sfinfo, 0, sizeof (sfinfo)) ;
+	if ((infile = sf_open (argv [argc - 2], SFM_READ, &sfinfo)) == NULL)
+	{	printf ("Error : Not able to open input file '%s'\n", argv [argc - 2]) ;
+		sf_close (infile) ;
+		exit (1) ;
+		} ;
+
+	if (sfinfo.channels == 1)
+	{	printf ("Input file '%s' already mono. Exiting.\n", argv [argc - 2]) ;
+		sf_close (infile) ;
+		exit (0) ;
+		} ;
+
+	/* Force output channels to mono. */
+	sfinfo.channels = 1 ;
+
+	if ((outfile = sf_open (argv [argc - 1], SFM_WRITE, &sfinfo)) == NULL)
+	{	printf ("Error : Not able to open output file '%s'\n", argv [argc - 1]) ;
+		sf_close (infile) ;
+		exit (1) ;
+		} ;
+
+	mix_to_mono (infile, outfile) ;
+
+	sf_close (infile) ;
+	sf_close (outfile) ;
+
+	return 0 ;
+} /* main */
+
+static void
+mix_to_mono (SNDFILE * infile, SNDFILE * outfile)
+{	double buffer [1024] ;
+	sf_count_t count ;
+	
+	while ((count = sfe_mix_mono_read_double (infile, buffer, ARRAY_LEN (buffer))) > 0)
+		sf_write_double (outfile, buffer, count) ;
+
+	return ;
+} /* mix_to_mono */
+
+static void
+usage_exit (void)
+{
+	puts ("\n"
+		"Usage :\n\n"
+		"    sndfile-mix-to-mono <input file> <output file>\n"
+		) ;
+	exit (0) ;
+} /* usage_exit */
