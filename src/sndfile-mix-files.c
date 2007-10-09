12222d816b5e5b7d5698803781b624e8f838d59a
Erik de Castro Lopo <erikd@mega-nerd.com>
2007-10-09 10:57:13 +1000

    Add file COPYING.GPLv3, change license to GPLv2 or GPLv3.

--- src/sndfile-mix-files.c
+++ src/sndfile-mix-files.c
@@ -1,9 +1,10 @@
 /*
 ** Copyright (C) 2007 Erik de Castro Lopo <erikd@mega-nerd.com>
 **
-** This program is free software; you can redistribute it and/or modify
+** This program is free software: you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
-** the Free Software Foundation; version 2 of the License only.
+** the Free Software Foundation, either version 2 or version 3 of the
+** License.
 **
 ** This program is distributed in the hope that it will be useful,
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -11,6 +12,5 @@
 ** GNU General Public License for more details.
 **
 ** You should have received a copy of the GNU General Public License
-** along with this program; if not, write to the Free Software
-** Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+** along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
