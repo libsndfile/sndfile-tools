a41d27bba096e631184f6116c2ffa063ad3f7cdf
Erik de Castro Lopo <erikd@mega-nerd.com>
2007-12-06 17:49:32 +1100

    Add file src/sndfile-truncate.c.

---
+++ src/sndfile-truncate.c
@@ -0,0 +1,22 @@
+/*
+** Copyright (C) 2007 Erik de Castro Lopo <erikd@mega-nerd.com>
+**
+** This program is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 2 or version 3 of the
+** License.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+**	Truncates a given file to the required length and then repairs the
+**	the headers. If the file contains a tailer it will be lost.
+*/
+
