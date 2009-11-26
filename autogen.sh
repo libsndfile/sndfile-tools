be02a9014ae89b6053f51b4f9624b6e6489c6918
Erik de Castro Lopo <erikd@mega-nerd.com>
2009-11-26 18:28:07 +1100

    autogen.sh : Clean up.

--- autogen.sh
+++ autogen.sh
@@ -126,6 +126,5 @@ echo "  autoconf"
 autoconf || exit 1
 
 cd $olddir
-$srcdir/configure --enable-maintainer-mode --enable-gcc-werror "$@" && echo
+$srcdir/configure -enable-gcc-werror "$@" && echo
 
-# (cd src && make genfiles)
