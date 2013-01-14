2db1b7208501d9fffba7ec5b2e9cd9fc487a1faf
Vitaly Driedfruit <driedfruit@mindloop.net>
2013-01-14 13:48:55 +0300

    tests/common_tests.c : Update copyright notice, to pass the pre-commit test.

--- tests/common_tests.c
+++ tests/common_tests.c
@@ -1,5 +1,6 @@
 /*
 ** Copyright (C) 2012 Erik de Castro Lopo <erikd@mega-nerd.com>
+** Copyright (C) 2013 driedfruit <driedfruit@mindloop.net>
 **
 ** This program is free software: you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
