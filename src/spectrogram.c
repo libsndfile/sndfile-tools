libsndfile/sndfile-tools

commit 9ca586748b2029b86a9b225178153953e0698f1e

src/spectrogram.c: Fix colour map calculation
Martin Guy did a lot of investigative work that showed that the
colour map calculation was producing a bunch of strange artifacts.

    https://github.com/erikd/sndfile-tools/issues/15

The simple fix here seems to fix at least some of those.

src/spectrogram.c
@@ -97,11 +97,6 @@ get_colour_map_value (float value, double spec_floor_db, unsigned char colour [3
 	float rem ;
 	int indx ;
 
-	if (value >= 0.0)
-	{	colour = map [0] ;
-		return ;
-		} ;
-
 	if (gray_scale)
 	{	/* "value" is a negative value in decibels.
 		 * black (0,0,0) is for <= -180.0,
@@ -129,6 +124,11 @@ get_colour_map_value (float value, double spec_floor_db, unsigned char colour [3
 		return ;
 		} ;
 
+	if (value >= 0.0)
+	{	colour [0] = colour [1] = colour [2] = 255 ;
+		return ;
+		} ;
+
 	value = fabs (value * (-180.0 / spec_floor_db) * 0.1) ;
 
 	indx = lrintf (floor (value)) ;
@@ -138,8 +138,8 @@ get_colour_map_value (float value, double spec_floor_db, unsigned char colour [3
 		exit (1) ;
 		} ;
 
-	if (indx >= ARRAY_LEN (map) - 1)
-	{	colour = map [ARRAY_LEN (map) - 1] ;
+	if (indx >= ARRAY_LEN (map))
+	{	colour [0] = colour [1] = colour [2] = 0 ;
 		return ;
 		} ;
 

commit 58dce1a43cc2f698055d1784bd59522f2bb801ab

src/spectrogram.c: Fix off-by-one error
The error was introduced in 9ca586748b.

src/spectrogram.c
@@ -133,7 +133,7 @@ get_colour_map_value (float value, double spec_floor_db, unsigned char colour [3
 		exit (1) ;
 		} ;
 
-	if (indx >= ARRAY_LEN (map))
+	if (indx >= ARRAY_LEN (map) - 1)
 	{	colour [0] = colour [1] = colour [2] = 0 ;
 		return ;
 		} ;