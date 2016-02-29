7eccdd7262488922eb068e5ec55a69541721962b
Erik de Castro Lopo <erikd@mega-nerd.com>
2016-02-29 20:08:17 +1100

    src/spectrum.c : Clean up allocations for `spectrum` struct
    Old code was allocating a single data structure and doing multiple
    allocations out of space at the end. This is neat because it makes
    free()ing the allocation so easy, but prevents tools like ASAN from
    finding memory access errors.
    
    Probably better to allow ASAN to do its job.
    
    Suggested-by: Martin Guy <martinwguy@gmail.com>

--- src/spectrum.c
+++ src/spectrum.c
@@ -18,15 +18,30 @@ spectrum *
 create_spectrum (int speclen, enum WINDOW_FUNCTION window_function)
 {	spectrum *spec ;
 
-	spec = calloc (1, sizeof (spectrum) + ((2 + 2 + 2 + 1) * speclen + 1) * sizeof (double)) ;
+	spec = calloc (1, sizeof (spectrum)) ;
+	if (spec == NULL)
+	{	printf ("%s : Not enough memory.\n", __func__) ;
+		exit (1) ;
+		} ;
 
 	spec->wfunc = window_function ;
 	spec->speclen = speclen ;
 
-	spec->time_domain = spec->data ;
-	spec->window = spec->time_domain + 2 * speclen ;
-	spec->freq_domain = spec->window + 2 * speclen ;
-	spec->mag_spec = spec->freq_domain + 2 * speclen ;	/* mag_spec has values from [0..speclen] inclusive */
+	/* mag_spec has values from [0..speclen] inclusive for 0Hz to Nyquist.
+	** time_domain has an extra element to be able to interpolate between
+	** samples for better time precision, hoping to eliminate artifacts.
+	*/
+	spec->time_domain = calloc (2 * speclen + 1, sizeof (double)) ;
+	spec->window = calloc (2 * speclen, sizeof (double)) ;
+	spec->freq_domain = calloc (2 * speclen, sizeof (double)) ;
+	spec->mag_spec = calloc (speclen + 1, sizeof (double)) ;
+	if (spec->time_domain == NULL
+		|| spec->window == NULL
+		|| spec->freq_domain == NULL
+		|| spec->mag_spec == NULL)
+	{	printf ("%s : Not enough memory.\n", __func__) ;
+		exit (1) ;
+		} ;
 
 	spec->plan = fftw_plan_r2r_1d (2 * speclen, spec->time_domain, spec->freq_domain, FFTW_R2HC, FFTW_MEASURE | FFTW_PRESERVE_INPUT) ;
 	if (spec->plan == NULL)
@@ -61,6 +76,10 @@ void
 destroy_spectrum (spectrum * spec)
 {
 	fftw_destroy_plan (spec->plan) ;
+	free (spec->time_domain) ;
+	free (spec->window) ;
+	free (spec->freq_domain) ;
+	free (spec->mag_spec) ;
 	free (spec) ;
 } /* destroy_spectrum */
 
